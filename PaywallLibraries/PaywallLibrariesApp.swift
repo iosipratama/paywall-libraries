d418667e839ab85e324c86ac65dada0514309ea0
Iosi Pratama <pratamaiosi@gmail.com>
2025-09-22 20:06:22 +0700

    Initial Commit

---
+++ PaywallLibraries/PaywallLibrariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaywallLibrariesApp.swift
+//  PaywallLibraries
+//
+//  Created by Iosi Pratama on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PaywallLibrariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
