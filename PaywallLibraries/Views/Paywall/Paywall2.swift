b65d18cd118801ebd53ec994aa1ecd74683ad2f3
Iosi Pratama <pratamaiosi@gmail.com>
2025-09-23 21:47:15 +0700

    start working on paywall 2

---
+++ PaywallLibraries/Views/Paywall/Paywall2.swift
@@ -0,0 +1,18 @@
+//
+//  Paywall2.swift
+//  PaywallLibraries
+//
+//  Created by Iosi Pratama on 23/09/25.
+//
+
+import SwiftUI
+
+struct Paywall2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Paywall2()
+}
