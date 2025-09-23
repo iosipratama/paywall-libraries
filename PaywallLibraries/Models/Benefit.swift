e972b557e50192b4ee804e3ee44930b4c02e5925
Iosi Pratama <pratamaiosi@gmail.com>
2025-09-23 07:41:03 +0700

    working on paywall 1

---
+++ PaywallLibraries/Models/Benefit.swift
@@ -0,0 +1,8 @@
+//
+//  Benefit.swift
+//  PaywallLibraries
+//
+//  Created by Iosi Pratama on 23/09/25.
+//
+
+import Foundation
---
+++ PaywallLibraries/Views/Shared/BenefitRow.swift
@@ -0,0 +1,18 @@
+//
+//  BenefitRow.swift
+//  PaywallLibraries
+//
+//  Created by Iosi Pratama on 23/09/25.
+//
+
+import SwiftUI
+
+struct BenefitRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BenefitRow()
+}
