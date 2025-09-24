35e0a3df2682757871769f8e32698b4e504fede9
Iosi Pratama <pratamaiosi@gmail.com>
2025-09-24 21:50:01 +0700

    create 3rd paywall

---
+++ PaywallLibraries/Views/Paywall/Paywall3.swift
@@ -0,0 +1,18 @@
+//
+//  Paywall3.swift
+//  PaywallLibraries
+//
+//  Created by Iosi Pratama on 24/09/25.
+//
+
+import SwiftUI
+
+struct Paywall3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Paywall3()
+}
