5b2ee279997f6acf8bb4a40e690b151429563131
Iosi Pratama <pratamaiosi@gmail.com>
2025-09-28 05:59:50 +0700

    fix sheet height

---
+++ PaywallLibraries/Views/Paywall/Paywall6.swift
@@ -0,0 +1,18 @@
+//
+//  Paywall6.swift
+//  PaywallLibraries
+//
+//  Created by Iosi Pratama on 27/09/25.
+//
+
+import SwiftUI
+
+struct Paywall6: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Paywall6()
+}
