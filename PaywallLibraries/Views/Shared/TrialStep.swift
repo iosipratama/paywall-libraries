9b9e61e8802db814274008e7760aff0ed297c472
Iosi Pratama <pratamaiosi@gmail.com>
2025-09-30 10:39:43 +0700

    add shared component for the trial timeline

---
+++ PaywallLibraries/Views/Shared/TrialStep.swift
@@ -0,0 +1,18 @@
+//
+//  TrialStep.swift
+//  PaywallLibraries
+//
+//  Created by Iosi Pratama on 30/09/25.
+//
+
+import SwiftUI
+
+struct TrialStep: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrialStep()
+}
