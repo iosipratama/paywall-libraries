264c91dca9c90975733424f69400d0f8d4cced68
Iosi Pratama <pratamaiosi@gmail.com>
2025-09-30 10:39:56 +0700

    start working on paywall 6

--- PaywallLibraries/Views/Paywall/Paywall6.swift
+++ PaywallLibraries/Views/Paywall/Paywall6.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct Paywall6: View {
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("How your trial works")
+            
+            VStack{
+                TrialStep(
+                    icon: "checkmark.circle.fill",
+                    title: "Today",
+                    description: "Start exploring premium features today!",
+                    isLast: false
+                    
+                )
+                
+                TrialStep(
+                    icon: "bell.fill",
+                    title: "Day 5",
+                    description: "Get a friendly reminder before the free trial ends",
+                    isLast: false
+                    
+                )
+                
+                TrialStep(
+                    icon: "star.fill",
+                    title: "Day 7",
+                    description: "You'll be billed after 7 days, cancel anytime before Jul 5th.",
+                    isLast: true
+                    
+                )
+            }
+            .padding()
+            
+            VStack {
+                Text("Unlimited access for 7 days")
+                Text("then $39.99 annually")
+            }
+            Button{
+                
+            } label : {
+                Text("Start Free Trial")
+                    .frame(height: 54)
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.white)
+                    .background(.purple)
+                    .clipShape(RoundedRectangle(cornerRadius: 13))
+            }
+            Link("View All Plans", destination: URL(string: "#")!)
+            
+            
+            
+        }
     }
 }
 
--- PaywallLibraries/Views/Shared/TrialStep.swift
+++ PaywallLibraries/Views/Shared/TrialStep.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct TrialStep: View {
+    let icon: String
+    let title: String
+    let description: String
+    let isLast: Bool
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .top){
+            VStack{
+                // Icon inside circle
+                Image(systemName: icon)
+                    .frame(width: 28, height: 28)
+                    .foregroundStyle(.white)
+                    .background(Circle().fill(.purple))
+                
+                // Vertical line
+                if !isLast {
+                    Rectangle()
+                        .fill(.purple)
+                        .frame(width: 2)
+                        .frame(maxHeight: 32)
+                    
+                }
+            }
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.headline)
+                    .bold()
+                Text(description)
+                    .font(.subheadline)
+                    
+                
+            }
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    TrialStep()
+    TrialStep(icon: "checkmark.circle.fill", title: "Today", description: "Start exploring premium features today!", isLast: true)
 }
